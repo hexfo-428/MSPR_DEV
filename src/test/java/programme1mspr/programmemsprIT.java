eedff8e2768edbd97a63b30668ee11e77325715f
Yann <Yann@LENOVO-YANN.groupe-pedago.loc>
2022-03-09 14:14:01 +0100

    Yann

--- src/test/java/programme1mspr/programmemsprIT.java
+++ src/test/java/programme1mspr/programmemsprIT.java
@@ -49,6 +49,7 @@ public class programmemsprIT {
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
         assertTrue(true);
+        
     }
 
    
